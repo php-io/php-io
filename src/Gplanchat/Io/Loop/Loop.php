309c5bb859dd63aafa7a5ce0df11f584ae462ca3
Gregory PLANCHAT <g.planchat@gmail.com>
2013-03-25 11:04:42 +0100

    Upgraded comments
    Signed-off-by: Grégory PLANCHAT <g.planchat@gmail.com>

--- src/Gplanchat/Io/Loop/Loop.php
+++ src/Gplanchat/Io/Loop/Loop.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * This file is part of php-io.
+ * This file is part of Gplanchat\Io.
  *
- * php-io is free software: you can redistribute it and/or modify it under the
+ * Gplanchat\Io is free software: you can redistribute it and/or modify it under the
  * terms of the GNU LEsser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * php-io is distributed in the hope that it will be useful,
+ * Gplanchat\Io is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with php-io.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Gplanchat\Io.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Grégory PLANCHAT <g.planchat@gmail.com>
  * @license Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl-3.0.txt)
